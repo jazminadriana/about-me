6b429e9195cd397d86d2fd36027bb4ef8db86890
Jazmín Hernández <li.jhernandezz@gmail.com>
2026-03-26 17:38:53 -0600

    feat: implement reusable Section component with props and custom styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
-import { useState } from 'react'
 import './App.css'
+import profileAvatar from './assets/images/profile-avatar.jpg'
+import journeyAvatar from './assets/images/journey-avatar.jpg'
+import stackAvatar from './assets/images/stack-avatar.jpg'
+import Section from './components/Section'
+
 
 function App() {
-  
+  return (
+    <div className='App'>
+        <h1 className='app-title'>Jazmín | Web Developer Jr</h1>
+        <Section 
+          title='About Me'
+          description='I am a passionate developer with experience in React and Node.js.'
+          image={profileAvatar}
+        />
+        <Section 
+          title='My Journey'
+          description='I started my journey in web development in 2020 and have been learning and growing ever since.'
+          image={journeyAvatar}
+        />
+        <Section 
+          title='Tech Stack'
+          description='I am proficient in JavaScript, React, Node.js, and have experience with databases like MongoDB.'
+          image={stackAvatar}
+        />
+    </div>
+  )
 }
+
+export default App
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,21 @@
-function Section() {
+import '../assets/styles/section.css'
+
+function Section(props) {
   return (
     <div className='section-container'>
+
       <img 
         className='section-image' 
-        src={require('../assets/profile-avatar.jpg')} 
-        alt='Profile Avatar' />
+        src={props.image} 
+        alt={`icon for ${props.title}`} />
+
+      <div className='section-content'>
+        <p className='section-title'>{props.title}</p>
+        <p className='section-description'>{props.description}</p>
+      </div>
+
     </div>
   )
 }
+
+export default Section
