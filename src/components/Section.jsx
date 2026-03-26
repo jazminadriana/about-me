313c9cda102f5735a0a12becb794213e7449b1a1
Jazmín Hernández <li.jhernandezz@gmail.com>
2026-03-26 12:24:04 -0600

    chore: setup project structure and assets

---
+++ src/components/Section.jsx
@@ -0,0 +1,10 @@
+function Section() {
+  return (
+    <div className='section-container'>
+      <img 
+        className='section-image' 
+        src={require('../assets/profile-avatar.jpg')} 
+        alt='Profile Avatar' />
+    </div>
+  )
+}
